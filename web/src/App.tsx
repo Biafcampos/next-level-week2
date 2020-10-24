1e1da8343a29cf272d204515803a196cd1ce1a53
Bianca <66906291+Biafcampos@users.noreply.github.com>
2020-10-24 10:39:53 -0300

    front-end web

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div >
+      <Header title = "hello world"/> 
+      <Header title = "falaaa dev!!!!!"/> 
+      <Header title = "eaiii rapeizeee estado!!!"/> 
 
-        <h1> Hello World</h1>
-      </header>
+        <h1> Conteudo da aplicação</h1>
+     
     </div>
   );
 }
